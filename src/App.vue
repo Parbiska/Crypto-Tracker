e435942999fcf29e7c87f2244780d7145f2a347e
Demid Zaynulin <demid.zaynulin@machinists.ru>
2025-11-04 00:39:28 +0800

    add basic logic for App

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="flex flex-col min-h-screen">
+    <Header />
+    
+    <main class="flex-1 container mx-auto px-4 py-6">
+      <RouterView />
+    </main>
+    
+    <Footer />
+  </div>
 </template>
 
 <style scoped></style>
